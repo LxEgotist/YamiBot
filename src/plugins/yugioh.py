ba51f789d81b2491ddbb31c30abccded06a05367
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-08-12 12:25:25 +0800

    Update yugioh.py

--- src/plugins/yugioh.py
+++ src/plugins/yugioh.py
@@ -1,14 +1,43 @@
 import json
 from nonebot import on_command
+from utils.utils_banList import banList
 from nonebot.adapters.cqhttp import Bot, Event
 
-ocg = on_command("ocg", priority=5)
-@ocg.handle()
+OCGSearch = on_command("ocg", priority=5)
+
+
+@OCGSearch.handle()
 async def _(bot: Bot, event: Event, state: dict) -> None:
-    msg = str(event.message).strip()
-    if len(msg) != 1:
-        await ocg.finish('你在输什么j8')
-    else:
-        name=msg[0]
-        url=f'https://www.ourocg.cn/search/{name}/'
-        await ocg.finish(url)
+    user = str(event.user_id)
+    group = str(event.group_id)
+
+    if not banList(user, group):
+        msg = str(event.message).strip()
+
+        if msg:
+            pass
+        else:
+            msg0 = "-==Yu-Gi-Oh!==-\n"
+            msg0 += "ocg:\n"
+            msg0 += "├search [keyword]\n"
+            msg0 += "└image [card ID]"
+
+            await OCGSearch.finish(msg0)
+
+        if not (len(msg) == 2 and (msg[0] == "search" or msg[0] == "image")):
+            await OCGSearch.finish('你在输什么j8')
+        else:
+            if msg[0] == "search":
+                key = msg[1]
+                url = f'https://www.ourocg.cn/search/{key}/'
+
+                await OCGSearch.finish(url)
+            elif msg[0] == "image":
+                Key = msg[1]
+                num = eval(Key)
+                if isinstance(num, int):
+                    url = f'https://storage.googleapis.com/ygoprodeck.com/pics/{Key}.jpg'
+                else:
+                    await OCGSearch.finish('你在输什么j8')
+
+
