7120ec69f736b4120605f7a66a3d0a8ec307dcd2
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-08-13 20:05:10 +0800

    new shabi*1

--- src/plugins/yugioh.py
+++ src/plugins/yugioh.py
@@ -1,4 +1,5 @@
 import json
+from urllib import parse, request
 from nonebot import on_command
 from utils.utils_banList import banList
 from utils.utils_img import compress_image, aio_download_pics
@@ -8,15 +9,15 @@ OCGSearch = on_command("ocg", priority=5)
 
 
 @OCGSearch.handle()
-async def _(bot: Bot, event: Event, state: dict) -> None:
+async def _(bot: Bot, event: Event, state: dict) -> None :
     user = str(event.user_id)
     group = str(event.group_id)
 
-    if not banList(user, group):
+    if not banList(user, group) :
         msg = str(event.message).strip().split(' ')
-        if msg:
+        if msg :
             pass
-        else:
+        else :
             msg0 = "-==Yu-Gi-Oh!==-\n"
             msg0 += "ocg:\n"
             msg0 += "├search [keyword]\n"
@@ -24,24 +25,22 @@ async def _(bot: Bot, event: Event, state: dict) -> None:
 
             await OCGSearch.finish(msg0)
 
-
-        if len(msg) < 2:
+        if len(msg) < 2 :
             await OCGSearch.finish('你在输什么j8')
-        else:
-            mo=msg[0]
-            key=msg[1]
-            if mo == "search":
+        else :
+            mo = msg[0]
+            key = msg[1]
+            if mo == "search" :
+                key = parse.quote(key)
                 url = f'https://www.ourocg.cn/search/{key}/'
-
                 await OCGSearch.finish(url)
-            elif mo == "image":
+            elif mo == "image" :
                 num = eval(key)
-                if isinstance(num, int):
+                if isinstance(num, int) :
                     url = f'https://storage.googleapis.com/ygoprodeck.com/pics/{key}.jpg'
-                    msg0=f'[CQ:image,file=file:///{await aio_download_pics(url)}]\n'
+                    msg0 = f'[CQ:image,file=file:///{compress_image(await aio_download_pics(url))}]\n'
                     await OCGSearch.finish(msg0)
-                else:
+                else :
                     await OCGSearch.finish('卡密都不知道查个锤子卡图')
-            else:
+            else :
                 await OCGSearch.finish('鬼知道出现了什么错误')
-
