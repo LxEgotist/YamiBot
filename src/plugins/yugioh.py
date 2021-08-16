0c471333091e76cb97153c9d8f277df324854d30
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-08-16 19:47:46 +0800

    修复了没有bug的bug

--- src/plugins/yugioh.py
+++ src/plugins/yugioh.py
@@ -9,38 +9,59 @@ OCGSearch = on_command("ocg", priority=5)
 
 
 @OCGSearch.handle()
-async def _(bot: Bot, event: Event, state: dict) -> None :
+async def _(bot: Bot, event: Event, state: dict) -> None:
     user = str(event.user_id)
     group = str(event.group_id)
 
-    if not banList(user, group) :
+    if not banList(user, group):
         msg = str(event.message).strip().split(' ')
-        if msg :
+        if msg:
+
             pass
-        else :
+        else:
             msg0 = "-==Yu-Gi-Oh!==-\n"
             msg0 += "ocg:\n"
-            msg0 += "├search [keyword]\n"
-            msg0 += "└image [card ID]"
+            msg0 += "搜索：- search [keyword]\n"
+            msg0 += "裁定：- rtd [keyword]\n"
+            msg0 += "查卡：- card [card ID]\n"
+            msg0 += "查图：- image [card ID]"
 
             await OCGSearch.finish(msg0)
+        if len(msg) < 2:
 
-        if len(msg) < 2 :
-            await OCGSearch.finish('你在输什么j8')
-        else :
+            await OCGSearch.finish('你🐎查啥都不知道害搁这查呢')
+        else:
             mo = msg[0]
             key = msg[1]
-            if mo == "search" :
+            if mo == "search":  # 搜索
                 key = parse.quote(key)
-                url = f'https://www.ourocg.cn/search/{key}/'
+                url = f'https://ygocdb.com/?search={key}/'
+
                 await OCGSearch.finish(url)
-            elif mo == "image" :
+            elif mo == "image":  # 查图
                 num = eval(key)
-                if isinstance(num, int) :
-                    url = f'https://storage.googleapis.com/ygoprodeck.com/pics/{key}.jpg'
+                if isinstance(num, int):
+                    url = f'https://cdn.233.momobako.com/ygopro/pics/{key}.jpg'
                     msg0 = f'[CQ:image,file=file:///{compress_image(await aio_download_pics(url))}]\n'
+
                     await OCGSearch.finish(msg0)
-                else :
+                else:
+
                     await OCGSearch.finish('卡密都不知道查个锤子卡图')
-            else :
-                await OCGSearch.finish('鬼知道出现了什么错误')
+            elif mo == "rtd":  # 裁定
+                key = parse.quote(key)
+                url = f'https://ocg-rule.readthedocs.io/zh_CN/latest/?rtd_search={key}'
+
+                await OCGSearch.finish(url)
+            elif mo == "card":  # 查卡
+                num = eval(key)
+                if isinstance(num, int):
+                    url = f'https://ygocdb.com/card/{key}'
+
+                    await OCGSearch.finish(url)
+                else:
+
+                    await OCGSearch.finish('卡密都不知道查个锤子卡')
+            else:
+
+                await OCGSearch.finish('你在输什么j8')
